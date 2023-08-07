3fb97074ef9e4424aca7cea9062441811f751205
patel <nisargapate21@gnu.ac.in>
2023-08-07 08:54:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_practical4_21012011094/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mad_practical4_21012011094
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+    val loginActivity:Button=findViewById(R.id)
+}
---
+++ app/src/main/java/com/example/mad_practical4_21012011094/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mad_practical4_21012011094
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+    val login:Button =
+}
